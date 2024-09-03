ce03bec7d85722b42336c7b798f43ddea0f78cb2
Erick <erickmogor21@gmail.com>
2024-09-03 10:14:26 +0100

    should be able to switch screens now with the saved Notes button.

--- app/src/main/java/com/example/mad700practicetest/MainActivity.java
+++ app/src/main/java/com/example/mad700practicetest/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,7 +16,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity
 {
-    static Button BTNexit, BTNsave;
+    static Button BTNexit, BTNsave, btnsavedNotes;
+    static TextView savedTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,6 +29,9 @@ public class MainActivity extends AppCompatActivity
 
         BTNexit = findViewById(R.id.btnExit);
         BTNsave = findViewById(R.id.btnSave);
+        btnsavedNotes = findViewById(R.id.btnsavedNotes);
+
+        savedTextView = findViewById(R.id.saveTextView);
 
 //        BTNsave.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -35,32 +40,33 @@ public class MainActivity extends AppCompatActivity
 //            }
 //        });
 
+        //exists the application
+        BTNexit.setOnClickListener(v->
+        {
+
+            Toast.makeText(this, "You have EXITED", Toast.LENGTH_LONG ).show();
+            System.out.println("You have Exited");
+        });
+
+        //saves the notes
+        BTNsave.setOnClickListener(v->
+        {
+            SharedPreferences storeText = getSharedPreferences("savedTEXTS", MODE_PRIVATE);
+            storeText = (SharedPreferences) savedTextView;
+            Toast.makeText(this, "Your notes are saved", Toast.LENGTH_LONG ).show();
+        });
+
+        //move from the main page to the new screen
+        btnsavedNotes.setOnClickListener(v->
+        {
+            Intent intent = new Intent(this, NewScreen.class);
+            startActivity(intent);
+            Toast.makeText(this, "Saved Notes", Toast.LENGTH_LONG ).show();
+        });
 
     }
 
 
-    //Button to exit the second page
-    public void exitClick(View view)
-    {
-        Toast.makeText(this, "You have EXITED", Toast.LENGTH_LONG ).show();
-        System.out.println("You have Exited");
-    }
-
-    //Button to save the text from the user
-    public void saveClick(View view)
-    {
-        SharedPreferences storeText = getSharedPreferences("savedTEXTS", MODE_PRIVATE);
-        Toast.makeText(this, "Your notes are saved", Toast.LENGTH_LONG ).show();
-
-    }
-
-    //Button to move from page 1 to page 2
-    public void savedNotesClick(View view)
-    {
-        Intent intent = new Intent(this, NewScreen.class);
-        startActivity(intent);
-        Toast.makeText(this, "Saved Notes", Toast.LENGTH_LONG ).show();
-    }
 }
 
 
--- app/src/main/java/com/example/mad700practicetest/NewScreen.java
+++ app/src/main/java/com/example/mad700practicetest/NewScreen.java
@@ -1,17 +1,26 @@
 package com.example.mad700practicetest;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewScreen extends AppCompatActivity
 {
+    Button btnExit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_newscreen);
+
+        btnExit = findViewById(R.id.btnExit);
+
+        btnExit.setOnClickListener(v->{});
+
     }
 }
